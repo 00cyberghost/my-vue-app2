b5906eb5a25862b0a342c4276dae64ad80e091fa
ozor clinton <00cyberghost@gmail.com>
2024-03-18 12:26:51 +0100

    commited

--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
-import './assets/main.css'
+// import './assets/main.css'
 
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+
+// import App from './App.vue'
+// import router from './router'
+
+// const app = createApp(App)
+
+// app.use(createPinia())
+// app.use(router)
+
+// app.mount('#app')
+ import './assets/main.css'
+import { createApp } from 'vue';
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
+import App from './App.vue';
 import router from './router'
 
-const app = createApp(App)
-
+const app = createApp(App);
 app.use(createPinia())
 app.use(router)
 
 
-    app.mount('#app')
-  
+// Mount the app if running in the browser
+if (import.meta.env.DEV) {
+  app.mount('#app');
+}
 
-
+// Export the app instance for library mode
+export default app;
